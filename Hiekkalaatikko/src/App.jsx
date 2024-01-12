5768b784a53fb99a6671cace48bf827ef127856e
elli kiiski <kiiski.elli@gmail.com>
2024-01-12 08:15:21 +0200

    ehdollinen renderöinti esimerkki

--- Hiekkalaatikko/src/App.jsx
+++ Hiekkalaatikko/src/App.jsx
@@ -1,5 +1,20 @@
 import { useState } from 'react'
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
@@ -20,13 +35,13 @@ const App = () => {
 
   return (
     <div>
-      {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {right}
-      <p>{allClicks.join(' ')}</p>
-
-      <p>total {total}</p>
+      <div>
+        {left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {right}
+        <History allClicks={allClicks} />
+      </div>
     </div>
   )
 }
