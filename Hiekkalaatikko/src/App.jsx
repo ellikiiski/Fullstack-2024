0dae90460de5f32f3d92ea214e0b73cffc0920c2
elli kiiski <kiiski.elli@gmail.com>
2024-01-12 07:58:29 +0200

    tapahtumakäsittely esimerkki

--- Hiekkalaatikko/src/App.jsx
+++ Hiekkalaatikko/src/App.jsx
@@ -1,25 +1,43 @@
-const Hello = ({ name, age }) => { // mistä noit propsit tietää laittaa nimen oikeeseen ja iän oikeeseen muuttujaan??
-  const bornYear = () => new Date().getFullYear() - age
+import { useState } from 'react'
 
+const Display = (props) => {
   return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born {bornYear()}</p>
-    </div>
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
   )
 }
 
 const App = () => {
-  const nimi = 'Pekka'
-  const ika = 10
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={nimi} age={ika} />
+      <Display counter={counter}/>
+
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />           
     </div>
   )
 }
