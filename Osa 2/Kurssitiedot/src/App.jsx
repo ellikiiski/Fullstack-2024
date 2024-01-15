d8f24a17db1eb85fda4918b6ca26fd4a26c69e23
elli kiiski <kiiski.elli@gmail.com>
2024-01-15 18:37:17 +0200

    osa 2 tehtävä 5

--- Osa 2/Kurssitiedot/src/App.jsx
+++ Osa 2/Kurssitiedot/src/App.jsx
@@ -1,62 +1,4 @@
-const Header = ({ text, size}) => {
-  if (size == 1) {
-    return (
-      <>
-        <h1>{text}</h1>
-      </>
-    )
-  }
-  return (
-    <>
-      <h2>{text}</h2>
-    </>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <>
-      <li>
-        {props.part} {props.exercises}
-      </li>
-    </>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      <ul>
-        {parts.map(part => 
-          <Part key={part.id} part={part.name} exercises={part.exercises} />  
-        )}
-      </ul>
-    </>
-  )
-}
-
-const Total = ({ parts }) => {
-  const sum = parts.reduce((accumulator, part) => accumulator + part.exercises, 0);
-
-  return (
-    <>
-      <p><b>Number of exercises {sum}</b></p>
-    </>
-  );
-};
-
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <div>
-        <Header text={course.name} size={2} />
-        <Content parts={course.parts} />
-        <Total parts={course.parts} />
-      </div>
-    </>
-  )
-}
+import Course from "./Course"
 
 const App = () => {
   const courses = [
@@ -106,7 +48,7 @@ const App = () => {
 
   return (
     <div>
-      <Header text={"Web dev curriculum"} size={1}/>
+      <h1>{"Web dev curriculum"}</h1>
       <ul>
         {courses.map(course => <Course key={course.id} course={course} />)}
       </ul>
