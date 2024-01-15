619d1f942b0a26a587880dcb64026c8e2fce93b6
elli kiiski <kiiski.elli@gmail.com>
2024-01-15 17:49:36 +0200

    osa 2 thetävä 1

---
+++ Osa 2/Kurssitiedot/src/App.jsx
@@ -0,0 +1,88 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <li>
+        {props.part} {props.exercises}
+      </li>
+    </>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      <ul>
+        {parts.map(part => 
+          <Part key={part.id} part={part.name} exercises={part.exercises} />  
+        )}
+      </ul>
+    </>
+  )
+}
+
+const Total = ({ parts }) => { // teinpähän nyt kuitenkin vaikkei tarvinnu
+  let sum = 0
+  for (let i = 0; i < parts.length; i++) {
+    sum += parts[i].exercises
+  }
+  return (
+    <>
+      <p>Number of exercises {sum}</p>
+    </>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Playing Tetris',
+        exercises: 8,
+        id: 4
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
