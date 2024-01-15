ef9b4aa963d80add8afd1285d7e1e3c7b69f985f
elli kiiski <kiiski.elli@gmail.com>
2024-01-15 20:22:45 +0200

    2b leikkejä leikitty

--- Hiekkalaatikko/src/App.jsx
+++ Hiekkalaatikko/src/App.jsx
@@ -1,16 +1,46 @@
-import Note from "./components/Note"
+import { useState } from 'react'
+import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
+  const [showAll, setShowAll] = useState(true)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>  
       <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} /> // haloo eiks tota key propsii tarvii erikseen määritellä tonne komponenttiin?
+        {notesToShow.map(note =>
+          <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
 
-export default App
+export default App 
