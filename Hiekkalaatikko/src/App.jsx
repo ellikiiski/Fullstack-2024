30827ed4545d8d6c7a59a3316e3313c46ab207c4
elli kiiski <kiiski.elli@gmail.com>
2024-01-15 17:23:41 +0200

    osan 2 leikkejä hiekkiksellä

--- Hiekkalaatikko/src/App.jsx
+++ Hiekkalaatikko/src/App.jsx
@@ -1,47 +1,14 @@
-import { useState } from 'react'
+import Note from "./components/Note"
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
+const App = ({ notes }) => {
   return (
     <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
-
-const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-    setTotal(left + right) // arvoksi total tulee 1 liian pieni, koska left on vielä sama vanha
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-    setTotal(left + right)
-  }
-
-  return (
-    <div>
-      <div>
-        {left}
-        <button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>
-        {right}
-        <History allClicks={allClicks} />
-      </div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} /> // haloo eiks tota key propsii tarvii erikseen määritellä tonne komponenttiin?
+        )}
+      </ul>
     </div>
   )
 }
