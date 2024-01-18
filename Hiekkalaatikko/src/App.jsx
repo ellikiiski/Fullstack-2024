d3a88f14d0f0c773aee15004fc875bd973cf6891
elli kiiski <kiiski.elli@gmail.com>
2024-01-18 07:33:36 +0200

    json leikkejä eli muistiinpanojen haku json-serveriltä hiekkikksellä

--- Hiekkalaatikko/src/App.jsx
+++ Hiekkalaatikko/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState(
-    'a new note...'
-  )
+const App = () => {
+
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  
+  useEffect(hook, []) // haluisin tietää miks tyhjä taulukko...
+
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
