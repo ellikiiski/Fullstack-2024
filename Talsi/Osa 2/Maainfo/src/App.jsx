e16fd254905e2d00525acd6deed8c9fa8ea8807c
elli kiiski <kiiski.elli@gmail.com>
2024-10-02 20:39:25 +0300

    aloitetaas taas alusta osasta 1... kuitenkin saman vuoden sisällä niin ei tarvii luoda uutta repoo

---
+++ Talsi/Osa 2/Maainfo/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react'
+
+const SeachBar = () => {
+
+  return (
+    <>
+      
+    </>
+  )
+
+}
+
+const App = () => {
+
+  return (
+    <div>
+      <h2>Country info</h2>
+
+    </div>
+  )
+}
+
+export default App
