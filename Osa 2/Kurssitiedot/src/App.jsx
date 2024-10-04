7f7fdfbddf6867729253a05148dfb31ceb1ce494
elli kiiski <kiiski.elli@gmail.com>
2024-10-04 23:33:38 +0300

    tehtävä 2.4 (taas)

--- Osa 2/Kurssitiedot/src/App.jsx
+++ Osa 2/Kurssitiedot/src/App.jsx
@@ -2,7 +2,7 @@
 const Header = ({ course }) => {
   return (
     <>
-      <h1>{course}</h1>
+      <h2>{course}</h2>
     </>
   )
 }
@@ -19,7 +19,7 @@ const Part = ({ part }) => {
 
 const Total = ({ parts }) => {
   const exercises = parts.map(part => part.exercises)
-  const total = exercises.reduce((acc, curr) => acc + curr, 0) // jälleen edelläkävijänä tein tän jo
+  const total = exercises.reduce((acc, curr) => acc + curr, 0)
   return (
     <>
       <p>TOTAL OF {total} EXERCISES</p>
@@ -47,31 +47,57 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  const courses_elements = courses.map(course => <Course key={course.id} course={course} />)  
 
   return (
     <div>
-      <Course course={course} />
+      <h1>Web development curriculum</h1>
+      {courses_elements}
     </div>
   )
 }
